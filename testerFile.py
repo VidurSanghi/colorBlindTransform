b3440ee5868daa8d576e0dcec841c59440301974
Vidur Sanghi <52296159+VidurSanghi@users.noreply.github.com>
2024-11-24 20:20:06 -0500

    Delete testerFile.py

--- testerFile.py
+++
@@ -1,7 +0,0 @@
-from cmu_graphics import *
-# Loads an image with the left-top at (50, 100).
-
-app.url = 'https://academy.cs.cmu.edu/static/media/project_10.472f439f.jpg'
-Image(app.url, 50, 100)
-
-cmu_graphics.run()
