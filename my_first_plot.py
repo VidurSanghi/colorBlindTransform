2597db3010b6ae0ee7edddc8a0a5fe90de5ae8fa
Vidur Sanghi <52296159+VidurSanghi@users.noreply.github.com>
2024-11-24 20:20:27 -0500

    Delete my_first_plot.py

--- my_first_plot.py
+++
@@ -1,5 +0,0 @@
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-image = mpimg.imread('/Users/vidursanghi/Desktop/colorBlindTransform/rubberduck112.jpg')
-plt.imshow(image)
-plt.show()
